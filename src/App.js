a64729057f8e5d1de3341e431c00d3a186e9b5b7
Xiodanny-Vasquez <xiodanny.vasquez@soy.sena.edu.co>
2024-10-25 23:51:32 -0500

    taller 4 de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// Ejercicio 1: Manejo de eventos
 
-function App() {
+// import React from "react";
+
+// function ButtonClick() {
+//   const manejoClick = () => {
+//     alert("¡Botón clickeado!");
+//   };
+//   return (
+//     <div>
+//       <button onClick={manejoClick}>haz click en el boton por favor</button>
+//     </div>
+//   );
+// }
+// export default ButtonClick;
+
+
+// Ejercicio 2: Creación y gestión de formularios
+
+// import React, { useState } from "react";
+
+// function Formulario() {
+//   const [nombre, setNombre] = useState("");
+
+//   const manejoCambio = (evento) => {
+//     setNombre(evento.target.value);
+//   };
+
+//   const manejoEnvio = (evento) => {
+//     evento.preventDefault();
+//     alert (`Nombre: ${nombre}`);
+//   };
+
+//   return (
+//       <form onSubmit={manejoEnvio}>
+//         <label>
+//           Nombre: 
+//           <input type="text" value={nombre} onChange={manejoCambio} />
+//         </label>
+//         <button type="submit" > Enviar </button>
+//       </form>
+//   );
+// };
+// export default Formulario;
+
+
+// Ejercicio 3:  Lifting State Up
+
+import React, {useState} from 'react';
+
+const App = () => {
+  const [numero, setNumero] = useState(''); 
+
+  const cambioNumero = (event) => {
+    setNumero(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Formulario</h1>
+      <input
+        type="number"
+        placeholder="Escribe un número"
+        onChange={cambioNumero}
+      />
+      <h1>resultado: {numero}</h1>
     </div>
   );
-}
+};
 
 export default App;
